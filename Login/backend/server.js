63420b35afd21424f906af376b64e59baf617512
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-20 18:50:55 +0530

    new commit

--- Login/backend/server.js
+++ Login/backend/server.js
@@ -3,6 +3,15 @@ import mongoose from 'mongoose'
 
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log(`Server is running on post: ${process.env.PORT}`);
+mongoose.connect(process.env.MONGODB_URI)
+.then(() =>{
+    console.log('MONGODB Connected Successfully.');
+    
+})
+.catch(() =>{
+    console.log(error);
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT}`);
 })

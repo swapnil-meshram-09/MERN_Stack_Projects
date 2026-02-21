9212e8ad2ea2e4aee7565ffcd235fede7b2d3c7d
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-21 18:41:25 +0530

    new commit

--- Login/frontend/src/App.jsx
+++ Login/frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import {Routes, Route, Link} from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
 function App(){
     return(
         <>
-        
+        <Routes>
+            <Route path='/' element={<Login />}></Route>
+            <Route path='/signup' element={<Signup />}></Route>
+        </Routes>
         </>
     )
 }

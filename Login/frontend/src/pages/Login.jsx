212e78e7ebc09d4487d5b355cd895137f984828e
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-21 19:04:33 +0530

    new commit

--- Login/frontend/src/pages/Login.jsx
+++ Login/frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
 function Login(){
+
+    function preventDefault(e){
+        e.preventDefault()
+    }
     return(
         <>
-        
+        <form action="" onSubmit={preventDefault}>
+            <h1>Login</h1>
+            <input type='text' placeholder='Enter username' onChange={(e) =>{e.target.value}}/> <br />
+            <input type='password' placeholder='Enter password' onChange={(e) =>{e.target.value}}/> <br />
+            <button>Login</button>
+        </form>
         </>
     )
 }

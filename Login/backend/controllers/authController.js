3960865afb459ff12a73b99962452eb1db044287
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-22 18:07:40 +0530

    new commit

--- Login/backend/controllers/authController.js
+++ Login/backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import express from 'express'
+import { login, signup } from '../controllers/authController.js'
 
 const router = express.Router()
 
-router.post('',)
-router.post('',)
+router.post('/login', login)
+router.post('/signup', signup)

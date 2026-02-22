21944e7ea7ae214ac85fdb2cb1464db33f1d4383
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-22 17:55:17 +0530

    new commit

--- Login/frontend/src/pages/Signup.jsx
+++ Login/frontend/src/pages/Signup.jsx
@@ -9,10 +9,10 @@ function Signup(){
 
         <h2>Sign Up</h2>
 
-        <input type='text' placeholder='Enter username' onChange={(e) =>{e.target.value}} />
-        <input type='email' placeholder='Enter Email' onChange={(e) =>{e.target.value}} />
-        <input type='password' placeholder='Enter Password' onChange={(e) =>{e.target.value}} />
-        <input type='password' placeholder='Enter Confirm Password' onChange={(e) =>{e.target.value}} />
+        <input type='text' placeholder='Enter username' onChange={(e) =>{e.target.value}} /> <br />
+        <input type='email' placeholder='Enter Email' onChange={(e) =>{e.target.value}} /> <br />
+        <input type='password' placeholder='Enter Password' onChange={(e) =>{e.target.value}} /> <br />
+        <input type='password' placeholder='Enter Confirm Password' onChange={(e) =>{e.target.value}} /> <br />
        
         <button>Sign Up</button>
 

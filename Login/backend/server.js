1467b23d74839b5a3dcdf227ddd54738d0df9f72
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-22 18:02:07 +0530

    new commit

--- Login/backend/server.js
+++ Login/backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
 connectDB()
+
+app.use(cors())
+app.use(express.json())
+
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server is running on port: ${process.env.PORT}`);

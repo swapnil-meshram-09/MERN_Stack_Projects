55102dcfc4addd1b0c94bfebb242c3e121785c15
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-23 18:34:08 +0530

    new commit

--- Login/frontend/src/pages/Login.jsx
+++ Login/frontend/src/pages/Login.jsx
@@ -1,7 +1,12 @@
 function Login(){
     return(
         <>
-
+        <form action="" onSumbit={handleForm}>
+            <h2>Login </h2>
+            <input type='text'      placeholder='Enter Username / Email'/>
+            <input type='password'  placeHolder='Enter Password'/>
+            <button>Login</button>
+        </form>
         </>
     )
 }

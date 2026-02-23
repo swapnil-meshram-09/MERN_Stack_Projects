a401635de0a2663fd970342819c84e4acc0d4d26
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-23 18:04:45 +0530

    new commit

--- Login/frontend/src/pages/Signup.jsx
+++ Login/frontend/src/pages/Signup.jsx
@@ -10,16 +10,16 @@ function Signup(){
         confirmPassword: ''
     });
 
-    function preventDefault(e){
+    function handleForm(e){
         e.preventDefault()
     }
     return(
         <>
-        <form action="" onSubmit={preventDefault}>
+        <form action="" onSubmit={handleForm}>
 
         <h2>Sign Up</h2>
 
-        <input type='text' placeholder='Enter username' onChange={(e) =>{e.target.value}} /> <br />
+        <input type='text' placeholder='Enter Username' onChange={(e) =>{e.target.value}} /> <br />
         <input type='email' placeholder='Enter Email' onChange={(e) =>{e.target.value}} /> <br />
         <input type='password' placeholder='Enter Password' onChange={(e) =>{e.target.value}} /> <br />
         <input type='password' placeholder='Enter Confirm Password' onChange={(e) =>{e.target.value}} /> <br />

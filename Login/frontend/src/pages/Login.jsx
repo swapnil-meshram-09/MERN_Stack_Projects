c8e2615fca85ff1b2785cf7101d65013e9825dff
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-23 18:38:48 +0530

    new commit

--- Login/frontend/src/pages/Login.jsx
+++ Login/frontend/src/pages/Login.jsx
@@ -3,6 +3,11 @@ import axios  from 'axios'
 
 function Login(){
 
+    const [ form, setForm ] = useState({
+        username:'',
+        password:''
+    })
+
     function handleChange(e){
 
     }
@@ -13,8 +18,8 @@ function Login(){
         <>
         <form action="" onSumbit={handleForm}>
             <h2>Login </h2>
-            <input type='text'      placeholder='Enter Username / Email' onChange={handleChange}/>
-            <input type='password'  placeHolder='Enter Password'         onChange={handleChange}/>
+            <input type='text'     name='username' placeholder='Enter Username / Email' onChange={handleChange}/>
+            <input type='password' name='password' placeholder='Enter Password'         onChange={handleChange}/>
             <button>Login</button>
         </form>
         </>

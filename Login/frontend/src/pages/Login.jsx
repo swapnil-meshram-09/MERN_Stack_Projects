5d1fd3589e75ed3493b9ae1233dae9a8495ba2aa
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-23 18:37:07 +0530

    new commit

--- Login/frontend/src/pages/Login.jsx
+++ Login/frontend/src/pages/Login.jsx
@@ -1,10 +1,17 @@
 function Login(){
+
+    function handleChange(e){
+        
+    }
+    function handleForm(e){
+        e.preventDefault()
+    }
     return(
         <>
         <form action="" onSumbit={handleForm}>
             <h2>Login </h2>
-            <input type='text'      placeholder='Enter Username / Email'/>
-            <input type='password'  placeHolder='Enter Password'/>
+            <input type='text'      placeholder='Enter Username / Email' onChange={handleChange}/>
+            <input type='password'  placeHolder='Enter Password'         onChange={handleChange}/>
             <button>Login</button>
         </form>
         </>

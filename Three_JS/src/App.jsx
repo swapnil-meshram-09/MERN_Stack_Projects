47b7a4bb88a61220e6d25c1380902e6aea670d68
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-24 18:30:05 +0530

    new commit

--- Three_JS/src/App.jsx
+++ Three_JS/src/App.jsx
@@ -1,22 +1,22 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
 
-function Box() {
-  return (
-    <mesh rotation={[10, 10, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
-}
+// function Box() {
+//   return (
+//     <mesh rotation={[10, 10, 0]}>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color="orange" />
+//     </mesh>
+//   );
+// }
 
-export default function App() {
-  return (
-    <Canvas style={{ height: "100vh" }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 2, 2]} />
-      <Box />
-      <OrbitControls />
-    </Canvas>
-  );
-}
+// export default function App() {
+//   return (
+//     <Canvas style={{ height: "100vh" }}>
+//       <ambientLight intensity={0.5} />
+//       <directionalLight position={[2, 2, 2]} />
+//       <Box />
+//       <OrbitControls />
+//     </Canvas>
+//   );
+// }

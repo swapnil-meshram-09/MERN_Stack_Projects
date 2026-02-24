a3ac26356fd3328070623eb6bd743078081c2ecf
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-24 18:27:25 +0530

    new commit

--- Three_JS/src/App.jsx
+++ Three_JS/src/App.jsx
@@ -1,2 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+
+function Box() {
+  return (
+    <mesh rotation={[10, 10, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}

3777b0ec567f7d43645cede680430c55022e10bd
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-25 18:23:10 +0530

    new commit

--- Login/backend/controllers/authController.js
+++ Login/backend/controllers/authController.js
@@ -1,17 +1,9 @@
 import user from '../models/user.js'
 export const signup = async() =>{
-    return(
-        <>
-        
-        </>
-    )
+    
 }
 
 
 export const login = async() =>{
-    return(
-        <>
-        
-        </>
-    )
+    
 }

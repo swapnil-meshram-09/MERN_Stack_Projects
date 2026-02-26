b880fe6213a1122c1beba90745902032c4846bf3
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-26 18:38:18 +0530

    new commit

--- Login/backend/controllers/authController.js
+++ Login/backend/controllers/authController.js
@@ -52,7 +52,8 @@ export const login = async() =>{
         }
 
         const token = jwt.sign(
-
+            {id: user._id},
+       
         )
     } catch(error){
         res.status(500).json({msg:'server error'})

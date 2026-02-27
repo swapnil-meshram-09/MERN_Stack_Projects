a6cd8ed4e50e5d518caf64bdf5b14ec46f440a5f
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-27 19:14:28 +0530

    new commit

--- Login/backend/server.js
+++ Login/backend/server.js
@@ -9,7 +9,11 @@ dotenv.config()
 const app = express()
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5137",
+  credentials: true
+}));
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
